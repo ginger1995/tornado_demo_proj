fc7266f94156362444ef4bf485dcd467995980c2
ginger1995 <jbw7@qq.com>
2016-12-07 11:45:39 +0800

    add some comments

--- handlers/index.py
+++ handlers/index.py
@@ -10,11 +10,14 @@ class IndexHandler(tornado.web.RequestHandler):
     def get(self):
         self.render("index.html")
 
+    # IndexHandler中的post方法对应着处理index.html中的post方式的请求
     def post(self):
+        # 先通过get_argument()方法活得前端index.html传过来的参数word的值，再根据这个值去数据库查询
         word = self.get_argument("word")
         word_item = mrd.select_table(
             table='wordbank', column='*', condition='word', value=word)
         if word_item == ():
+            # 若根据word的值查询到不为空再向前端由write()方法传回该word的值，由前端的ajax函数去处理进一步的请求
             self.write("nodata")
         else:
             self.write(word)
--- handlers/search.py
+++ handlers/search.py
@@ -11,4 +11,5 @@ class SearchHandler(tornado.web.RequestHandler):
         word = self.get_argument("word")
         word_item = mrd.select_table(
             table="wordbank", column="*", condition="word", value=word)
+        # 后端数据查询完毕后，跳转到word.html页面并附带上查询到的word_item数据（元组）
         self.render("word.html", worditem=word_item)
